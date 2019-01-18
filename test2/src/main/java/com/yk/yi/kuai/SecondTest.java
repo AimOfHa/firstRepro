89f8e09e00b96e47886ac705b92a850388798740
AimOfHa <2281382321@qq.com>
2019-01-18 15:56:24 +0800

    测试2

--- test2/src/main/java/com/yk/yi/kuai/SecondTest.java
+++ test2/src/main/java/com/yk/yi/kuai/SecondTest.java
@@ -1,7 +1,7 @@
 package com.yk.yi.kuai;
 
-public class myFirst {
+public class SecondTest {
     public static void main(String[] args) {
-        System.out.println("第一个项目");
+        System.out.println("...................");
     }
 }
